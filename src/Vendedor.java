6585ea834abd7d6ae9b5a02271e334543019557d
Mari <marianaromerog2@gmail.com>
2024-05-07 12:48:59 -0500

    Herencia y Polimorfismo - Implementación del Método calcularSalario() en Clase Vendedor por Mariana

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,6 +1,19 @@
-public class Vendedor extends Empleado{
+public class Vendedor extends Empleado {
+    private double comision;
 
-    public Vendedor(String nombre, double salario) {
-        super (nombre,salario);
+    public Vendedor(String nombre, double salarioBase, double comision) {
+        super(nombre, salarioBase);
+        this.comision = comision;
+    }
+
+    @Override
+    public double calcularSalario() {
+
+        return SalarioBase;
+    }
+
+    public double calcularSalario(double ventas) {
+        return SalarioBase + (comision * ventas);
     }
 }
+
