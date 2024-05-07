398de79ac668c98acbb008380bbe80d4e5c1bcf6
Mari <marianaromerog2@gmail.com>
2024-05-07 11:52:50 -0500

    Herencia y Polimorfismo - Implementación de Clase Empleado por Mariana

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,8 @@
 public class Empleado implements Trabajador{
+    private String Nombre;
+    private double Salario;
+
+
     @Override
     public void realizarTarea() {
         System.out.println("Realizando tarea laboral");
