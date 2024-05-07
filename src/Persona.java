2d0998048d3e807d56ae36aacf3572f81888b569
Mari <marianaromerog2@gmail.com>
2024-05-07 07:05:23 -0500

    Encapsulamiento - Implementación de métodos para manipulación de propiedades por Mariana

--- src/Persona.java
+++ src/Persona.java
@@ -2,4 +2,28 @@ public class Persona {
     private String Nombre;
     private String FechaDeNacimiento;
     private Integer Edad;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return FechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        FechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public Integer getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
 }
